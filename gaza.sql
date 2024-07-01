b3f22c2aac9817ddd126b88490a3529ada7e93c0
Daniel <122654917+dtecoder46@users.noreply.github.com>
2024-07-01 16:15:11 -0400

    Update gaza.sql with new death count data

--- gaza.sql
+++ gaza.sql
@@ -7,4 +7,6 @@ insert into gaza_deaths values ('2024-06-14', 37266);
 
 insert into gaza_deaths values ('2024-06-16', 37296);
 
+insert into gaza_deaths values ('2024-06-30', 37877);
+
 select * from gaza_deaths
