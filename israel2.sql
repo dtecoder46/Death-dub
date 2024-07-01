af8943fc1ccbefdb7a7a65b6e3d8068ac471cfab
Daniel <122654917+dtecoder46@users.noreply.github.com>
2024-07-01 16:15:47 -0400

    Update israel2.sql

--- israel2.sql
+++ israel2.sql
@@ -7,5 +7,7 @@ insert into israel_deaths values ('2024-06-14', 1139);
 
 insert into israel_deaths values ('2024-06-16', 1139);
 
+insert into israel_deaths values ('2024-06-30', 1139);
+
 select * from israel_deaths
 where date_of_count = '2024-06-14';
